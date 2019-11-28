ab50d40ef8f89f26e93511753e9a93a4d801e11e
Phil Tietjen <tietjen.philip@gmail.com>
2019-11-28 18:43:35 -0500

    build: setup styleguid config

---
+++ src/docs/introduction.md
@@ -0,0 +1,5 @@
+CBC UI is an experimental library of presentational components to be used across applications using ReactJS. It is intended to work as building blocks.
+
+We hope to deliver an easy to use component API that can be easily extendable to fit the functional and design needs for multiple platforms and audiences.
+
+This is a 2019 innovation sprint project that can either take off to be implemented into a larger design system, influence directions and tech for future React based projects, or simply be an experimental playground for approaching a react ui library. 
---
+++ styleguide.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+
+module.exports = {
+  assetsDir: "images/",
+  pagePerSection: true,
+  styles: {
+    StyleGuide: {
+      "@global body": {
+        fontFamily: "Open Sans,sans-serif"
+      }
+    }
+  },
+  theme: {
+    borderRadius: 10,
+    color: {
+      base: "#333",
+      light: "#767676",
+      lightest: "#ccc",
+      link: "#1673b1",
+      linkHover: "#e90",
+      focus: "rgba(22, 115, 177, 0.25)",
+      border: "#e8e8e8",
+      name: "#690",
+      type: "#905",
+      error: "#c00",
+      baseBackground: "#fff",
+      codeBackground: "#f5f5f5",
+      sidebarBackground: "#f5f5f5",
+      ribbonBackground: "#e90",
+      ribbonText: "#fff"
+    }
+  },
+  sections: [
+    {
+      name: "Introduction",
+      content: "src/docs/introduction.md"
+    },
+    {
+      name: "Components",
+      components: "src/components/**/[A-Z]*.{ts,tsx}",
+      exampleMode: "expand", // 'hide' | 'collapse' | 'expand'
+      usageMode: "expand" // 'hide' | 'collapse' | 'expand'
+    }
+  ],
+  styleguideComponents: {
+    Wrapper: path.join(__dirname, 'src/components/Theme/Theme')
+  },
+  propsParser: require("react-docgen-typescript").withDefaultConfig().parse,
+  ignore: ['**/__tests__/**', '**/*.test.{js,jsx,ts,tsx}', '**/*.spec.{js,jsx,ts,tsx}', '**/constants.{js,jsx,ts,tsx}']
+};
