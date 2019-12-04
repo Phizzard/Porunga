4c8b9191e442bf52819c949928f2872ab00ccbf3
Keith Brewster <keith.brewster@cbc.ca>
2019-12-04 09:57:30 -0500

    feat(theme): add ThemeConfig interface to use with Theme component

--- src/components/Theme/Theme.tsx
+++ src/components/Theme/Theme.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { ThemeConfig } from '../../types'
 import { ThemeProvider } from 'emotion-theming'
 import { defaultTheme } from '../../theme'
 
-interface IThemeProps {
+interface ThemeProps {
   /** Content inside the Theme Wrapper */
+  theme: ThemeConfig
   children: React.ReactNode
 }
 
-export const Theme = ({ children }: IThemeProps) => (
+export const Theme = ({ children }: ThemeProps) => (
   <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
 )
 export default Theme
---
+++ src/theme/default.ts
@@ -0,0 +1,36 @@
+import { ThemeConfig } from '../types'
+
+const defaultTheme: ThemeConfig = {
+  root: {
+    colors: {
+      brand: '#FF0000',
+      'accent-1': '',
+      'accent-2': '',
+      'accent-3': '',
+      'accent-4': '',
+      'neutral-1': '',
+      'neutral-2': '',
+      'neutral-3': '',
+      'neutral-4': '',
+      error: '',
+      warning: '',
+      success: '',
+      unknown: '',
+      disabled: '',
+      'light-1': '',
+      'light-2': '',
+      'light-3': '',
+      'light-4': '',
+      'light-5': '',
+      'light-6': '',
+      'dark-1': '',
+      'dark-2': '',
+      'dark-3': '',
+      'dark-4': '',
+      'dark-5': '',
+      'dark-6': '',
+    },
+  },
+}
+
+export default defaultTheme
