e1ea73469a97803c227b6e0ccdf5a7f6a4a9ca31
Keith Brewster <keith.brewster@cbc.ca>
2019-12-04 09:59:21 -0500

    refactor(button): refactor Button component to use useTheme hook

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,30 @@
 import React from 'react'
+import { useTheme } from 'emotion-theming'
+
+import { ThemeConfig } from '../../types'
+import { get } from '../../utils/get'
 import { Container } from './style'
 
 /**
  * Button properties
  */
-export interface IButtonProps {
+export interface ButtonProps {
   /** Content inside the button element */
+  backgroundColor: string
   children?: React.ReactNode
 }
 
 /**
  * Button
  */
-export const Button = ({ children }: IButtonProps) => (
-  <Container>{children}</Container>
-)
+export const Button = ({ backgroundColor, children }: ButtonProps) => {
+  const theme: ThemeConfig = useTheme()
+
+  const buttonProps = {
+    backgroundColor: backgroundColor || get(theme, 'root', 'colors', 'brand'),
+  }
+
+  return <Container {...buttonProps}>{children}</Container>
+}
 
 export default Button
--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -1,27 +1,22 @@
 import styled from '@emotion/styled'
 
-type ButtonProps = {
-  theme?: {
-    primaryColor?: string
-  }
+interface ButtonProps {
+  backgroundColor?: string
 }
 
-export const Container = styled.button`
-  color: ${(props: ButtonProps) =>
-    props && props.theme && props.theme.primaryColor};
+export const Container = styled.button<ButtonProps>`
+  color: ${p => p.backgroundColor};
   padding: 0.6rem 1.1rem;
   font-size: 1.1rem;
   font-weight: 500;
-  border: 3px solid
-    ${(props: ButtonProps) => props && props.theme && props.theme.primaryColor};
+  border: 3px solid ${p => p.backgroundColor};
   border-radius: 24px;
   cursor: pointer;
   background-color: #f7fafc;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
   transition: 0.2s;
+  color: #fff;
   :hover {
-    background-color: ${(props: ButtonProps) =>
-      props && props.theme && props.theme.primaryColor};
-    color: #fff;
+    background-color: ${p => p.backgroundColor};
   }
 `
