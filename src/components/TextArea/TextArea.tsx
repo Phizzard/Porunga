fb2bc94f37932c76860763ec55775618ba4fd91a
Ryan Choi <ryan.choi@cbc.ca>
2019-12-10 16:05:17 -0500

    fix(textarea): update the type and description of Props & Methods

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -9,13 +9,19 @@ import { Container } from './style'
  * TextArea properties
  */
 export interface TextAreaProps {
-  /** Content inside the textarea element */
+  /** A borderColor identifier */
   borderColor?: Palette
+  /** Specifies the visible number of lines in a text area */
   rows?: number
+  /** Specifies the visible width of a text area */
   cols?: number
+  /** Specifies the maximum number of characters allowed in the text area */
   maxlength?: number
+  /** Specifies that a text area should be disabled */
   disabled?: boolean
+  /** Specifies a short hint that describes the expected value of a text area */
   placeholder?: string
+  /** Content inside the TextArea element */
   children?: React.ReactNode
 }
 
