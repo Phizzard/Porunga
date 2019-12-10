392e69f70cd3a0aac0ed446f4facc72a3ca25405
AMIR HONARPOUR <amir.honarpour@cbc.ca>
2019-12-10 15:46:40 -0500

    feat(checkbox): add checked prop

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { useTheme } from 'emotion-theming'
 import cx from 'classnames'
 
@@ -11,9 +11,10 @@ import { Container, Input, CustomCheckbox } from './style'
  */
 export interface CheckboxProps {
   /** Content inside the checkbox element */
-  onChange?(isChecked: boolean, event: object): any
+  onChange?(isChecked: boolean): any
   id?: string
   name?: string
+  checked?: boolean
   disabled?: boolean
   className?: string
   backgroundColor?: string
@@ -27,23 +28,33 @@ export const Checkbox = ({
   onChange,
   id,
   name,
+  checked,
   disabled,
   className,
   backgroundColor,
   children,
 }: CheckboxProps) => {
-  const [isChecked, setIsChecked] = useState(false)
+  const [isChecked, setIsChecked] = useState(checked || false)
   const theme: ThemeConfig = useTheme()
 
   const checkboxProps = {
     backgroundColor: backgroundColor || get(theme, 'root', 'colors', 'brand'),
   }
 
-  const handleChange = (event: object) => {
-    const newState = !isChecked
-    setIsChecked(newState)
-    if (onChange) onChange(newState, event)
-  }
+  const handleChange = useCallback(() => {
+    if (checked === undefined) {
+      const newState = !isChecked
+      setIsChecked(newState)
+      if (onChange) onChange(newState)
+    }
+  }, [checked, onChange, isChecked])
+
+  useEffect(() => {
+    if (checked !== undefined) {
+      setIsChecked(checked)
+      if (onChange) onChange(checked)
+    }
+  }, [checked, onChange])
 
   return (
     <Container
