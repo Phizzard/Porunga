7942759043869d29bf2014c1ed64b883bb3c20de
Phil Tietjen <tietjen.philip@gmail.com>
2020-02-14 16:22:26 -0500

    feat: setup declaration scripts

--- src/components/Button/__tests__/Button.test.tsx
+++ src/components/Button/__tests__/Button.test.tsx
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer'
 import Button from '../Button'
 
 it('renders correctly', () => {
-  const component = renderer.create(<Button />)
+  const component = renderer.create(<Button label="test" />)
 
   const tree = component.toJSON()
   expect(tree).toMatchSnapshot()
