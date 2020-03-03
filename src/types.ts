7836df72c84af500dd4c2705fdb752b1938c6fcd
Phil Tietjen <tietjen.philip@gmail.com>
2020-03-03 09:50:58 -0500

    feat(types): add percentage size values

--- src/types.ts
+++ src/types.ts
@@ -35,7 +35,14 @@ export type Sizes =
   | 'large'
   | 'xlarge'
   | 'xxlarge'
+  | '1/2'
+  | '1/3'
+  | '2/3'
+  | '1/4'
+  | '2/4'
+  | '3/4'
   | 'full'
+  | 'auto'
   | 'unset'
 
 export type Weight =
