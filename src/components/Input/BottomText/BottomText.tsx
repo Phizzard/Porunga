f70d649cb568d01b0452e0d23baf294a0cdce134
Keith Brewster <keith.brewster22@gmail.com>
2020-10-23 23:27:49 -0400

    refactor(input): add ability to pass input/container/label/text props

--- src/components/Input/BottomText/BottomText.tsx
+++ src/components/Input/BottomText/BottomText.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { StyledBottomText } from './style'
 
 interface BottomTextProps {
+  /** Content inside the BottomText element */
   children?: React.ReactNode
+  /** Any extra properties to pass to the bottom text */
+  props?: any
 }
 
-const Label = ({ children }: BottomTextProps) => {
-  return <StyledBottomText>{children}</StyledBottomText>
+const Label = ({ children, ...props }: BottomTextProps) => {
+  return <StyledBottomText {...props}>{children}</StyledBottomText>
 }
 
 export default Label
--- src/components/Input/Label/Label.tsx
+++ src/components/Input/Label/Label.tsx
@@ -2,16 +2,30 @@ import React from 'react'
 import { StyledLabel } from './style'
 
 interface LabelProps {
+  /** The text for the label */
   label?: string
+  /** Whether it should display as required */
   required?: boolean
+  /** The input id to match it to */
   inputId?: string
+  /** The label identifier */
   id?: string
+  /** Whether the label should be hidden */
   hideLabel?: boolean
+  /** Any extra properties to pass to the label */
+  props?: any
 }
 
-const Label = ({ id, label, required, inputId, hideLabel }: LabelProps) => {
+const Label = ({
+  id,
+  label,
+  required,
+  inputId,
+  hideLabel,
+  ...props
+}: LabelProps) => {
   return (
-    <StyledLabel htmlFor={inputId} id={id} hideLabel={hideLabel}>
+    <StyledLabel htmlFor={inputId} id={id} hideLabel={hideLabel} {...props}>
       {label}
       {required && '*'}
     </StyledLabel>
